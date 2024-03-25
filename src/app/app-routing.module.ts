c9143bd5df123f7668e40eb58e9e65620a350745
yago <yagocam5@gmail.com>
2024-03-25 18:46:55 -0300

    criando rota login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { TodolistComponent } from './todolist/todolist.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: TodolistComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { TodolistComponent } from './todolist/todolist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TodolistComponent
   ],
   imports: [
     BrowserModule,
